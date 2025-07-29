8d867680e342a92fb81964833af4c88ff00fdae7
Suhani <suhjain@Suhanis-MacBook-Pro.local>
2025-07-29 10:57:06 +0530

    test for tabindex

--- libs/blocks/article-feed/article-feed.js
+++ libs/blocks/article-feed/article-feed.js
@@ -409,7 +409,7 @@ async function buildFilter(type, tax, block, config) {
   const options = document.createElement('ul');
   options.classList.add('filter-options');
   options.setAttribute('data-type', type);
-  options.setAttribute('role', 'group');
+  // options.setAttribute('role', 'group');
   options.setAttribute('aria-label', `${tax.getCategoryTitle(type)} filters`);
 
   const category = tax.getCategory(tax[`${type.toUpperCase()}`]);
